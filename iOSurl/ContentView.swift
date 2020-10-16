335027b61733959b6ef2f7aa4bc4a4ca34825c9c
Mike Chirico (https://mchirico.github.io/) <mchirico@gmail.com>
2020-10-16 16:12:00 -0400

    Initial Commit

---
+++ iOSurl/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iOSurl
+//
+//  Created by Mike Chirico on 10/16/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ iOSurl/iOSurlApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOSurlApp.swift
+//  iOSurl
+//
+//  Created by Mike Chirico on 10/16/20.
+//
+
+import SwiftUI
+
+@main
+struct iOSurlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
